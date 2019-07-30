0b0094cd52e1cc1ead397d7785ba16b0a3eed77e
Anderson Ismael <23392311+aicouto@users.noreply.github.com>
2019-07-30 09:57:13 -0300

    M  download/download.php

--- download/download.php
+++ download/download.php
@@ -2,15 +2,16 @@
 //Criado por Anderson Ismael
 //30 de Julho de 2019
 
-function download($url, $method = 'GET') {
+function download($url) {
     $useragent='Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:68.0) Gecko/20100101 Firefox/68.0';
     $ch = curl_init();
     //you might need to set some cookie details up (depending on the site)
     //curl_setopt($ch, CURLOPT_TIMEOUT, 1);
     curl_setopt($ch, CURLOPT_URL,$url); //set the url we want to use
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_TIMEOUT, 1000);    
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); 
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 1000);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
     curl_setopt($ch, CURLOPT_USERAGENT, $useragent); //set our user agent
     $result= curl_exec ($ch); //execute and get the results
     if($result){
@@ -21,3 +22,4 @@ function download($url, $method = 'GET') {
     curl_close ($ch);
 }
 ?>
+
